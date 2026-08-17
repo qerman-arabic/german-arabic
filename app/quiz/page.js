6cc791d4d9a9e8199d5105237b6b2ff41a66f51d
moayad <moayad.ahmad2014@gmail.com>
2026-08-17 15:37:39 +0300

    fix options shuffle

--- app/quiz/page.js
+++ app/quiz/page.js
@@ -8,7 +8,7 @@ const LEVELS = ['A1', 'A2', 'B1', 'B2'];
 function shuffle(arr) {
   const a = [...arr];
   for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * i + 1);
+    const j = Math.floor(Math.random() * (i + 1));
     [a[i], a[j]] = [a[j], a[i]];
   }
   return a;
